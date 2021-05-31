bc9a4cdc581f321bebd854a104a1bd15df9ec1b2
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-31 09:23:15 +0530

    Java Script Hacking

--- hack.js
+++ hack.js
@@ -1,1 +1,12 @@
-from js import getElementByID
+function senddata1(){
+  document.getElementById("postDiv").style.color="red";
+}
+function senddata2(){
+  document.getElementById("privateDiv").style.color="blue";
+}
+function senddata3(){
+  document.getElementById("checkDiv").style.color="redandgreen";
+}
+function senddata4(){
+  document.getElementById("publishDiv").style.color="green";
+}
